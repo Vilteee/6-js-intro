c3cc6c86db2aa97fa2ed543a9024f972bb07ebef
PavasarioSniegas <vilte.kar@gmail.com>
2023-04-27 11:49:18 +0300

    strings

---
+++ js/03-string.js
@@ -0,0 +1,36 @@
+console.clear();
+
+const kabute = '';
+const kabute1 = "";
+
+console.log(kabute);
+
+
+//sveiki, <vardas>!
+const user1 = 'jonas';
+
+const hi1 = 'Sveiki,' + ' ' + user1 + '!';
+
+console.log(hi1);
+
+const personName = 'vardenis';
+const personLastName = 'pavardenis';
+
+const personHi = 'Hi, I am ' + personName + ' ' + personLastName + 'and I like You';
+
+console.log(personHi);
+
+
+const kabute777 = " Vienguba (') ir" + 'dviguba ("") kabutes.';
+console.log(kabute777);
+
+const color = 'red';
+
+console.log(color);
+
+
+
+
+
+
+
