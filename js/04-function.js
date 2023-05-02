6bab775d6770e10a30e6aabb03ac8f3cc833762e
PavasarioSniegas <vilte.kar@gmail.com>
2023-05-02 09:55:28 +0300

    function

---
+++ js/04-function.js
@@ -0,0 +1,35 @@
+console.clear();
+
+function sum(first, second ) {
+
+
+    console.log('>>>', first, second)
+
+    return first+second;
+
+
+}
+
+const s1 = sum(2, 2);
+
+console.log(s1);
+
+const s2 = sum(7, 5);
+
+console.log(s2);
+
+function hi (name) {
+
+    console.log(name);
+
+    return `Hi, my name is ${name} !`;
+
+}
+
+console.log(hi('Petras'));
+console.log(hi('Maryte'));
+console.log(hi('Jonas'));
+console.log(hi('Ona'));
+
+
+
