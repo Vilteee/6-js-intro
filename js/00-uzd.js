19dd2612572ff157cd93919dcbd49575d4c34c64
PavasarioSniegas <vilte.kar@gmail.com>
2023-05-04 10:12:13 +0300

    if commit

---
+++ js/00-uzd.js
@@ -0,0 +1,82 @@
+
+// Kintamųjų inicijavimas
+
+// Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+// Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+// Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+// Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+
+let number1 = 1;
+console.log(number1);
+
+let number2 = 222;
+console.log(number2);
+
+let number3 = -45;
+console.log(number3);
+
+let name = "petras";
+console.log(name);
+
+let surname = 'petrauskas';
+console.log(surname);
+
+let pet = 'dog';
+console.log(pet);
+
+const playersNumbers = [3, 5, 44, 16, 38];
+console.log(playersNumbers);
+
+const fruits = ['grapes', 'apples', 'bananas', 'mango', 'kiwi'];
+console.log(fruits);
+
+// Veiksmai su kintamaisiais
+
+// Susumuoti visus skaičiaus tipo kintamuosius
+// Rezultatą išvesti į console
+// Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+// Rezultatą išvesti į console
+// Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+// 1-2+3-4+5
+// Rezultatą išvesti į console
+// Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
+
+function sumOfNumber(number) {
+
+    let sum = 0;
+    sum += number[0];
+    sum += number[1];
+    sum += number[2];
+    sum += number[3];
+    sum += number[4];
+
+    return sum;
+}
+
+console.log(sumOfNumber(playersNumbers));
+ 
+
+function joinOfWords (word) {
+
+    return `${word[0]} ${word[1]} ${word[2]} ${word[3]} ${word[4]}`;
+
+}
+
+console.log(joinOfWords(fruits));
+
+
+function plusMinusLogic (number) {
+    let value = 0;
+
+  
+
+    return  `${number[0]} - ${number[1]} + ${number[2]} - ${number[3]} + ${number[4]}`;
+
+}
+
+console.log(plusMinusLogic(playersNumbers));                                                                                                                                                                 
---
+++ js/10-if.js
@@ -0,0 +1,106 @@
+
+console.clear();
+
+if ( 7 < 10 ) {
+    console.log('7 < 10');
+} else {
+    console.log('7 ne < 10');
+}
+
+const userAge = 10;
+const agelimit = 18;
+
+if (userAge >= agelimit) {
+    console.log('uzeik...');
+} else {
+
+    console.log(`dar palauk ${agelimit - userAge} metų...`);
+}
+
+const diena = 1;
+
+if (diena === 10) {
+    console.log('pirmadienis');
+
+} else if ( diena === 2){
+    console.log('antradienis');
+} else if ( diena === 3){
+    console.log('treciadienis');
+} else if ( diena === 4){
+    console.log('ketvirtadienis');
+} else if ( diena === 5){
+    console.log('penktadienis');
+} else if ( diena === 6){
+    console.log('sestadienis');
+} else if ( diena === 7){
+    console.log('sekmadienis');
+} else {
+    console.log('tokios dienos nera');
+}
+
+const arSvieciaSaule = false;
+const arLyja = false;
+
+let kaDaryti = 'nezinau';
+
+if (arSvieciaSaule) {
+kaDaryti = 'eisim i kiema';
+if (arLyja) {
+    kaDaryti = 'ziurim i vaivorykste ;o';
+} else {
+    kaDaryti = 'deginames pleziuke B-)'
+}
+} else {
+    kaDaryti = 'kodinsim...;-(';
+    if(arLyja) {
+        kaDaryti = 'koks skirtumas... vis vien busiu namie';
+    } else {
+        kaDaryti = 'stebim zvaigzdes';
+    }
+}
+
+console.log(kaDaryti);
+
+
+const day = 2;
+
+if(day === 1) {
+    console.log('pirm');
+} else {
+    if( day === 2) {
+        console.log('ant');
+    } else {
+        console.log('nieko nesakau');
+    }
+}
+
+const dd = 1;
+let kokiaDiena = 'nezinau';
+
+if (dd === 1) {
+kokiaDiena = 'pirmadienis';
+}
+if (dd === 2) {
+    kokiaDiena = 'antradienis';
+}
+if (dd === 3) {
+    kokiaDiena = 'treciadienis';
+} 
+
+const siandien = 1;
+
+
+const dienuPavadinimai = [
+    'pirmad', 
+    'antrad', 
+    'treciad',
+    'ketvirtad'
+]
+
+const savaitesDiena = dienuPavadinimai[siandien - 1];
+
+if(savaitesDiena === undefined) {
+    console.log('tokiod dienos nera');
+} else {
+    console.log(savaitesDiena);
+}
