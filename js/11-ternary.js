5431c317d7ca14d434dc522aa8cc70e3265d3f17
PavasarioSniegas <vilte.kar@gmail.com>
2023-05-04 11:42:53 +0300

    ternary array

---
+++ js/11-ternary.js
@@ -0,0 +1,15 @@
+
+
+const leidimas = 10 >= 18? 'pinametis' : 'nepilnametis';
+
+console.log(leidimas);
+
+let approve = '';
+
+if (10 >= 18 ) {
+    approve = 'pilnametis';
+} else {
+    approve = 'nepilnametis';
+}
+
+console.log(approve);
